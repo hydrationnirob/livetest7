7fdb1e1ba955d006ed31fea2b5e48c4af386fc73
Md Rakibull Haque <rakibnirob100@gmail.com>
2023-06-19 23:41:23 +0600

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
+import 'SecondPage.dart';
+
 void main() {
-  runApp(const CounterApp());
+  runApp(MaterialApp(
+    home: CounterApp(),
+  ));
 }
 
 class CounterApp extends StatefulWidget {
@@ -17,29 +21,29 @@ class _CounterAppState extends State<CounterApp> {
   void _incrementCounter() {
     setState(() {
       _counter++;
-      if (_counter ==5) {
+      if (_counter == 5) {
         showDialog(
-            context: context,
-            builder: (context) {
-              return AlertDialog(
-                  title: const Text('Alert'),
-                  content: const Text('Congratulations! You reached 10!'),
-                  actions: [
-                  TextButton(
+          context: context,
+          builder: (BuildContext context) {
+            return AlertDialog(
+              title: const Text('Alert'),
+              content: const Text('Congratulations! You reached 5!'),
+              actions: [
+                TextButton(
                   onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text('OK'),
-              ),
+                    Navigator.of(context).pop();
+                  },
+                  child: const Text('OK'),
+                ),
               ],
-              );
-
-
-
-      }
-
-      else if (_counter == 10) {
-        Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondPage()));
+            );
+          },
+        );
+      } else if (_counter == 10) {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const SecondPage()),
+        );
       }
     });
   }
@@ -52,52 +56,46 @@ class _CounterAppState extends State<CounterApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Counter App'),
-          centerTitle: true,
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                'Counter Value:',
-                style: TextStyle(fontSize: 20),
-              ),
-              Text(
-                '$_counter',
-                style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Expanded(
-                    flex: 2, // Increase the flex value for the increment button
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.green),
-                      ),
-                      onPressed: _incrementCounter,
-                      child: const Text('Increment'),
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Counter App'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Counter Value:',
+              style: TextStyle(fontSize: 20),
+            ),
+            Text(
+              '$_counter',
+              style: const TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 2,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.green),
+                    onPressed: _incrementCounter,
+                    child: const Text('Increment'),
                   ),
-                  const SizedBox(width: 16), // Add some spacing between the buttons
-                  Expanded(
-                    flex: 1, // Keep the default flex value for the decrement button
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all(Colors.red),
-                      ),
-                      onPressed: _decrementCounter,
-                      child: const Text('Decrement'),
-                    ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.red),
+                    onPressed: _decrementCounter,
+                    child: const Text('Decrement'),
                   ),
-                ],
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
